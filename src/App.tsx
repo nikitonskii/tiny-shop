f3847b09fdd0dd293548f2f3d56e96c7357cfdf2
workstation 1 <workstation1@workstations-Mac-mini.local>
2021-08-27 16:01:20 +0300

    add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,13 @@ import React from "react";
 import logo from "./logo.svg";
 import "./app.scss";
 
-import SignIn from "./pages/signIn";
-import SignUp from "./pages/signUp";
+import Router from "./router";
 
 const App: React.FC = (): JSX.Element => {
   return (
     <div className="app">
       <img src={logo} className="app-logo" alt="logo" />
-      <SignIn />
-      <SignUp />
+      <Router />
     </div>
   );
 };
